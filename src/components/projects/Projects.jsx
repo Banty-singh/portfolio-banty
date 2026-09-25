b35458133da15904e27205c09c97a40145672bc1
Banty-singh <bantysingh4754@gmail.com>
2026-09-26 01:20:27 +0530

    adding some project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -9,10 +9,22 @@ const projects = [
     link: 'https://portfolio-banty.netlify.app/'
   },
   {
+    title: 'CA Digitalization Platform',
+    description: 'A full-stack web application designed for Chartered Accountants to manage client login authentication, invoice processing, and document management.',
+    tech: ['Node.js', 'Express.js', 'EJS', 'MongoDB'],
+    link: 'https://cad-chartered-accountants-digitaliz.vercel.app/'
+  },
+  {
+    title: 'AbHiMaaN.ai',
+    description: 'A personalized AI assistant web application built with React and integrated with the Google Gemini API to deliver interactive chat responses, prompt suggestions, and context management.',
+    tech: ['React', 'JavaScript', 'CSS', 'Gemini API'],
+    link: 'https://abhimaan.vercel.app/'
+  },
+  {
     title: 'ToDo App',
     description: 'A task management app with CRUD operations, using local storage and React hooks.',
-    tech: ['Html','CSS' , 'JavaScript'],
-    link: '#'
+    tech: ['Html', 'CSS', 'JavaScript'],
+    link: 'https://my-todo-virid-six.vercel.app/'
   },
   {
     title: 'Weather Dashboard',
@@ -24,32 +36,38 @@ const projects = [
     title: 'Tic Tac Toe – Web Game',
     description: 'Tic Tac Toe is a simple web-based game created using HTML, CSS, and JavaScript.',
     tech: ['HTML', 'CSS', 'JavaScript'],
-    link: 'apptic-tac-toe.netlify.app'
+    link: 'https://tic-tac-toe-gilt-kappa-61.vercel.app/'
+  },
+  {
+    title: 'Tic Tac Toe – Using React',
+    description: 'Tic Tac Toe is a simple web-based game created using React and css for its web design.',
+    tech: ['React', 'CSS'],
+    link: 'https://tic-tac-toe-react-two-gamma.vercel.app/'
   }
 ];
 
 const Projects = () => {
   return (
     <>
-    <div id='Projects'>
-    <section className="projects-section">
-      <h1>Projects</h1>
-      <div className="projects-boxes">
-        {projects.map((project, index) => (
-          <div className="project-card" key={index}>
-            <h2>{project.title}</h2>
-            <p>{project.description}</p>
-            <div className="tech-tags">
-              {project.tech.map((tag, idx) => (
-                <span key={idx}>{tag}</span>
-              ))}
-            </div>
-            <a href={project.link} target="_blank" rel="noopener noreferrer">View Project</a>
+      <div id='Projects'>
+        <section className="projects-section">
+          <h1>Projects</h1>
+          <div className="projects-boxes">
+            {projects.map((project, index) => (
+              <div className="project-card" key={index}>
+                <h2>{project.title}</h2>
+                <p>{project.description}</p>
+                <div className="tech-tags">
+                  {project.tech.map((tag, idx) => (
+                    <span key={idx}>{tag}</span>
+                  ))}
+                </div>
+                <a href={project.link} target="_blank" rel="noopener noreferrer">View Project</a>
+              </div>
+            ))}
           </div>
-        ))}
+        </section>
       </div>
-    </section>
-    </div>
     </>
   );
 };
